486b23303f080ca8d6f4f725101c38980f9b0fc7
idelgador <idelgador@choucairtesting.com>
2023-07-08 20:37:46 -0500

    Se realiza la conexión a la base de datos del modulo login y registro

--- app/src/main/java/com/java/kingquiz/LoginActivity.java
+++ app/src/main/java/com/java/kingquiz/LoginActivity.java
@@ -5,24 +5,77 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.java.kingquiz.database.DatabaseHelper;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextUsername;
+    private EditText editTextPass;
+    private Button buttonIniciarS;
+    //private Button buttonVolver;
+
+    private DatabaseHelper databaseHelper;
+
+    String[] roles={"Estudiante","Profesor","Trabajo"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
+        editTextUsername = findViewById(R.id.et_username);
+        editTextPass = findViewById(R.id.et_password);
+        buttonIniciarS = findViewById(R.id.btn_login);
+       // buttonVolver = findViewById(R.id.buttonVolver);
+        databaseHelper = new DatabaseHelper(this);
+
+       /** Spinner spin1 = (Spinner) findViewById(R.id.spinner_roles);
+        spin1.setOnItemSelectedListener(this);
+
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,roles);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);**/
+
+
+
         Button btn_login = (Button) findViewById(R.id.btn_login);
         //Se realiza la acción de escuchar cuando el boton es clickeado
         btn_login.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                //Cuando se hace click, se llamada este metodo
+                //Declaramos estas variables de tipo texto y les damos el valor del texto ingresado en los EditText
+                String username = editTextUsername.getText().toString();
+                String contrasena = editTextPass.getText().toString();
 
-                iniciarSesion();
+                //Declaramos esta variable de tipo booleano enviándole los textos para ejecutar el método "Validar", en la
+                // clase DataBaseHelper, lo que hace es validar que efectivamente se hayan registrado esos datos a la base de datos y devolver un true or false
+                boolean iniciarSesionExitoso = databaseHelper.Validar(username, contrasena);
+
+                if (iniciarSesionExitoso) {
+                    iniciarSesion();
+                    //Si se devuelve un true, que significa que los datos sí estaban registrados en la base de datos, entonces ejecutará estas funciones
+
+                    //Esta sección establece la variable "Loggeado" del MainActivity como verdadero, lo que significa que se ha iniciado sesión correctamente
+                    //setLoggeado(true);
+
+                    //Esta sección envía el número de cédula ingresado en el EditTextCed y devuelve la cadena del nombre almacenado en la base de datos en la misma fila que la cédula
+                    String nombre = databaseHelper.obtenerNombre(username);
+
+                    //Esta sección crea una cadena formada por el nombre recibido del método anterior y envía la cadena al MainActivity para mostrarla en un TextView
+                  //  String mensajeBienvenida = "Bienvenido, " + nombre;
+                    //setWelcomeMessage(mensajeBienvenida);
+                    //Imprime un Toast con la misma cadena de arriba
+                   // Toast.makeText(LoginActivity.this, mensajeBienvenida, Toast.LENGTH_SHORT).show();
+                    //finish(); // Devuelve al MainActivity
+                } else {
+                    //Si se devuelve un false entonces mostrará este Toast
+                    Toast.makeText(LoginActivity.this, "No se ha podido iniciar sesión", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -37,8 +90,6 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
-
-
 
     }
 
--- app/src/main/java/com/java/kingquiz/Register.java
+++ app/src/main/java/com/java/kingquiz/Register.java
@@ -1,14 +1,64 @@
 package com.java.kingquiz;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.java.kingquiz.database.DatabaseHelper;
+
 public class Register extends AppCompatActivity {
+
+    private EditText editTextUsername;
+    private EditText editTextNombres;
+    private EditText editTextMail;
+    private EditText editTextPass;
+    private Button buttonRegistrarS;
+   // private Button buttonVolver;
+
+    private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_layout);
+
+        editTextUsername = findViewById(R.id.et_username);
+        editTextNombres = findViewById(R.id.et_name);
+        editTextMail = findViewById(R.id.et_email);
+        editTextPass = findViewById(R.id.et_password);
+        buttonRegistrarS = findViewById(R.id.btn_registrarse);
+       // buttonVolver = findViewById(R.id.buttonVolver);
+
+        databaseHelper = new DatabaseHelper(this);
+
+        buttonRegistrarS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = editTextUsername.getText().toString();
+                String nombre = editTextNombres.getText().toString();
+                String correo = editTextMail.getText().toString();
+                String contrasena = editTextPass.getText().toString();
+                //Envía los datos ingresados en los edittext y devuelve un booleano
+                //Devuelve true si se cargaron los datos correctamente, y false en caso contrario
+                boolean guardadoExitoso = databaseHelper.Registrar(username, nombre, correo, contrasena);
+                if (guardadoExitoso) {
+                    Toast.makeText(Register.this, "Registrado correctamente", Toast.LENGTH_SHORT).show();
+                    finish(); // Regresa al MainActivity
+                } else {
+                    Toast.makeText(Register.this, "No se ha podido guardar correctamente", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        /**buttonVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // Regresa al MainActivity
+            }
+        });**/
     }
 }
